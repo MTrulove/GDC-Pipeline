bfe884e6dfe6aec31df8899cdcd5da541f9b2cab
Meghan Kimball <meghan13@midway2-login2.rcc.local>
2019-06-20 16:53:00 -0500

    First commit

---
+++ gdc-vl.sh
@@ -0,0 +1,28 @@
+mkdir -p ~/.local/bin
+
+dir=$(mktemp -d)
+cd "$dir"
+
+### biobambam ver. 2.0.57
+conda install -c bioconda biobambam=2.0.57
+
+### bwa ver. 0.7.15
+conda install -c bioconda  bwa=0.7.15
+
+### samtools ver. 1.1
+conda install -c bioconda samtools=1.1
+
+### picard ver. 2.5.0
+conda install -c bioconda picard=2.5.0
+
+### somatic-sniper ver. 1.0.5.0
+conda install -c bioconda somatic-sniper=1.0.5.0
+
+
+### varscan ver. 2.4.0
+conda install -c bioconda varscan=2.4.0
+
+### GATK ver. 4.0.4.0
+conda install -c bioconda GATK4=4.0.4.0
+
+
