97ca3d553e6399b9de2cc96941b19566554247b7
Meghan Kimball <meghan.a.kimball@gmail.com>
2019-07-13 03:14:53 +0800

    annotated steps in co-cleaning workflow

--- gdc-stcc.sh
+++ gdc-stcc.sh
@@ -1,13 +1,13 @@
 #eq Co-Cleaning Command Line Parameters
-#Step one
+#Step one - RealignTargetCreator
 java -jar GenomeAnalysisTK.jar -T RealignerTargetCreator -R "/home/users/industry/chicago-university/meghan/254f697d-310d-4d7d-a27b-27fbf767a834" -known <known_indels.vcf> [ -I <input.bam> ] -o <realign_target.inervals>
 
-#Step two
+#Step two - IndelRealigner
 java -jar GenomeAnalysisTK.jar -T IndelRealigner -R "/home/users/industry/chicago-university/meghan/254f697d-310d-4d7d-a27b-27fbf767a834" -known <known_indels.vcf> -targetIntervals <realign_target.intervals> --noOriginalAlignmentTags [ -I <input.bam> ] -nWayOut <output.map>
 
-#Step three
+#Step three - BaseRecalibrator; dbSNP v.144
 java -jar GenomeAnalysisTK.jar -T BaseRecalibrator -R "/home/users/industry/chicago-university/meghan/254f697d-310d-4d7d-a27b-27fbf767a834" -I <input.bam> -knownSites <dbsnp.vcf> -o <bqsr.grp>
 
-#Step four
+#Step four - PrintReads
 java -jar GenomeAnalysisTK.jar -T PrintReads -R "/home/users/industry/chicago-university/meghan/254f697d-310d-4d7d-a27b-27fbf767a834" -I <input.bam> --BQSR <bqsr.grp> -o <output.bam>
 
