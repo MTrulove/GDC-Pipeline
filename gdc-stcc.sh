fb4eee317852da312c66c123e563897452c8c2b3
Meghan Kimball <meghan.a.kimball@gmail.com>
2019-07-18 06:14:26 +0800

    alignment workflow fixed

--- gdc-stcc.sh
+++ gdc-stcc.sh
@@ -1,6 +1,6 @@
-#eq Co-Cleaning Command Line Parameters
+#eq Co-Cleaning Command Line Parameters - Uses tumor and normal bam files
 #Step one - RealignTargetCreator
-java -jar GenomeAnalysisTK.jar -T RealignerTargetCreator -R "/home/users/industry/chicago-university/meghan/254f697d-310d-4d7d-a27b-27fbf767a834" -known <known_indels.vcf> [ -I <input.bam> ] -o <realign_target.inervals>
+java -jar GenomeAnalysisTK.jar -T RealignerTargetCreator -R "/home/users/industry/chicago-university/meghan/254f697d-310d-4d7d-a27b-27fbf767a834" -known <known_indels.vcf> [ -I <input.bam> ] -o <realign_target.intervals>
 
 #Step two - IndelRealigner
 java -jar GenomeAnalysisTK.jar -T IndelRealigner -R "/home/users/industry/chicago-university/meghan/254f697d-310d-4d7d-a27b-27fbf767a834" -known <known_indels.vcf> -targetIntervals <realign_target.intervals> --noOriginalAlignmentTags [ -I <input.bam> ] -nWayOut <output.map>
