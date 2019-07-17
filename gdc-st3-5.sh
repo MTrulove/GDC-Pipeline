4c76425dc41ef83c4798b0b5390aebf6e9678eba
Meghan Kimball <meghan.a.kimball@gmail.com>
2019-07-18 06:14:26 +0800

    alignment workflow fixed

--- gdc-st3-5.sh
+++ gdc-st3-5.sh
@@ -1,12 +1,9 @@
 #step three
-java -jar picard.jar SortSam CREATE_INDEX=true INPUT=<input.bam> OUTPUT=<output
-.bam> SORT_ORDER=coordinate VALIDATION_STRINGENCY=STRICT
+java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" SortSam CREATE_INDEX=true INPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S1Norm.bam" OUTPUT=S3S1Norm.bam SORT_ORDER=coordinate VALIDATION_STRINGENCY=STRICT
 
 #step four
-java -jar picard.jar MergeSamFiles ASSUME_SORTED=false CREATE_INDEX=true [INPUT
-= <input.bam>] MERGE_SEQUENCE_DICTIONARIES=false OUTPUT=<output_path> SORT_ORDE
-R=coordinate USE_THREADING=true VALIDATION_STRINGENCY=STRICT
+java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" MergeSamFiles ASSUME_SORTED=false CREATE_INDEX=true I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Norm.bam" I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Norm.bam" MERGE_SEQUENCE_DICTIONARIES=false OUTPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUPUT/Sample1/Normal/S1Norm.bam" SORT_ORDER=coordinate USE_THREADING=true VALIDATION_STRINGENCY=STRICT
 
 #step five
-java -jar picard.jar MarkDuplicates CREATE_INDEX=true INPUT =<input.bam> VALIDA
-TION_STRINGENCY=STRICT
+java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" MarkDuplicates CREATE_INDEX=true I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Norm.bam" OUTPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S5-OUPUT/Sample1/Normal/S5S1Norm.bam" M=S5S1NormDup.txt VALIDATION_STRINGENCY=STRICT
+
