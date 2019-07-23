5f4cf4119098b7ea7a6ab496c8ac6647ac333eef
meghankimball <meghan.a.kimball@gmail.com>
2019-07-24 04:20:10 +0800

    MuSE and SomaticSniper work

--- gdc-somaticsniper.sh
+++ gdc-somaticsniper.sh
@@ -1,4 +1,1 @@
-bam-somaticsniper "/home/users/industry/chicago-university/meghan/miniconda3/bin/bam-somaticsniper" -q 1 -L -G -Q 15 -s 0.01 -T 0.85 -N 2 -r 0.001 -n NORMAL -t TUMOR -F vcf -f "/home/users/industry/chicago-university/meghan/GDC-Pipeline/GRCh38.d1.vd1.fa" 
-"/home/users/industry/chicago-university/meghan/GDC-Pipeline/S5-OUTPUT/Sample1/Tumor/S5S1Tumor.bam" 
-"/home/users/industry/chicago-university/meghan/GDC-Pipeline/S5-OUTPUT/Sample1/Normal/S5S1Norm.bam" 
-"/home/users/industry/chicago-university/meghan/GDC-Pipeline/SS-OUTPUT/S1-Variants.vcf"
+/home/users/industry/chicago-university/meghan/miniconda3/bin/bam-somaticsniper -q 1 -L -G -Q 15 -s 0.01 -T 0.85 -N 2 -r 0.001 -n NORMAL -t TUMOR -F vcf -f "/home/users/industry/chicago-university/meghan/GDC-Pipeline/GRCh38.d1.vd1.fa" "/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUPUT/Sample1/Tumor/S1Tumor.bam" "/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUPUT/Sample1/Normal/S1Norm.bam" "/home/users/industry/chicago-university/meghan/GDC-Pipeline/SS-OUTPUT/S1-Variants.vcf"
--- gdc-svcw-muse.sh
+++ gdc-svcw-muse.sh
@@ -1,9 +1,9 @@
 #MuSE Variant Call Command
 #MuSE call
-"/home/users/industry/chicago-university/meghan/miniconda3/bin/MuSE" MuSE call -f "/home/users/industry/chicago-university/meghan/GDC-Pipeline/GRCh38.d1.vd1.fa" -r "/home/users/industry/chicago-university/meghan/GDC-Pipeline/S5-OUTPUT/Sample1/Tumor/S5S1Tumor.bam" "/home/users/industry/chicago-university/meghan/GDC-Pipeline/S5-OUTPUT/Sample1/Normal/S5S1Norm.bam"  -O "/home/users/industry/chicago-university/meghan/GDC-Pipeline/MuSEOutput/MuSE-S1.txt"
+"/home/users/industry/chicago-university/meghan/miniconda3/bin/MuSE" call -f "/home/users/industry/chicago-university/meghan/GDC-Pipeline/GRCh38.d1.vd1.fa" "/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUPUT/Sample1/Tumor/S1Tumor.bam" "/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUPUT/Sample1/Normal/S1Norm.bam" -O "/home/users/industry/chicago-university/meghan/GDC-Pipeline/MuSEOutput/MuSE-S1.txt"
 
 #MuSE sump
 
-#MuSe sump -I <intermediate_muse_call.txt> -E -D <dbsnp_known_snp_sites.vcf> -O <muse_variants.vcf>
+"/home/users/industry/chicago-university/meghan/miniconda3/bin/MuSE" sump -I "/home/users/industry/chicago-university/meghan/GDC-Pipeline/MuSEOutput/MuSE-S1.txt" -E -D "/home/users/industry/chicago-university/meghan/GDC-Pipeline/common_all_20180418.vcf" -O "/home/users/industry/chicago-university/meghan/GDC-Pipeline/MuSEOutput/MuSE-S1variants.vcf"
 
 #Note -E is used for WXS and -G for WGS data
