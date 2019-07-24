7e1748288b84bac07fcd6e39c806728b4f18d4d1
meghankimball <meghan.a.kimball@gmail.com>
2019-07-25 05:27:18 +0800

    Varscan works correctly

--- gdc-st3-5.sh
+++ gdc-st3-5.sh
@@ -1,9 +1,9 @@
 #step three
-java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" SortSam CREATE_INDEX=true INPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S1Tumor.bam" OUTPUT=S3S1Tumor.bam SORT_ORDER=coordinate VALIDATION_STRINGENCY=STRICT
+java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" SortSam CREATE_INDEX=true INPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S1Norm.bam" OUTPUT=S3S1Norm.bam SORT_ORDER=coordinate VALIDATION_STRINGENCY=STRICT
 
 #step four
-java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" MergeSamFiles ASSUME_SORTED=false CREATE_INDEX=true I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Tumor.bam" I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Tumor.bam" MERGE_SEQUENCE_DICTIONARIES=false OUTPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUPUT/Sample1/Tumor/S1Tumor.bam" SORT_ORDER=coordinate USE_THREADING=true VALIDATION_STRINGENCY=STRICT
+java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" MergeSamFiles ASSUME_SORTED=false CREATE_INDEX=true I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Norm.bam" I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Norm.bam" MERGE_SEQUENCE_DICTIONARIES=false OUTPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUTPUT/Sample1/Normal/S1Norm.bam" SORT_ORDER=coordinate USE_THREADING=true VALIDATION_STRINGENCY=STRICT
 
 #step five
-java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" MarkDuplicates CREATE_INDEX=true I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S3S1Tumor.bam" OUTPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S5-OUTPUT/Sample1/Tumor/S1-5-tumor.bam" M=S5S1TumorDup.txt VALIDATION_STRINGENCY=STRICT
+java -jar "/home/users/industry/chicago-university/meghan/miniconda3/share/picard-2.5.0-2/picard.jar" MarkDuplicates CREATE_INDEX=true I="/home/users/industry/chicago-university/meghan/GDC-Pipeline/ST4-OUTPUT/Sample1/Normal/S1Norm.bam" OUTPUT="/home/users/industry/chicago-university/meghan/GDC-Pipeline/S5-OUTPUT/Sample1/Tumor/S1-5-normal.bam" M=S5S1TumorDup.txt VALIDATION_STRINGENCY=STRICT
 
